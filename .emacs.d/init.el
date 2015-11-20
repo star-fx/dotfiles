31d101f65b40133cb3c22cfc91b768c5e1f79208
star_fx <star.fx27@gmail.com>
2015-11-20 11:04:55 +0800

    add more config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,19 +3,54 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; backup no
+;; backup off 
 (setq make-backup-files nil)
-;; autosave no
+;; autosave off 
 (setq auto-save-default nil)
 ;; alarm off
 (setq ring-bell-function 'ignore)
 ;; welcome page off
 (setq inhibit-startup-message t)
+;; line number format
+(setq linum-format "%d ")
+;; line number on
+(global-linum-mode t)
+;; toolbar off
+(tool-bar-mode -1)
+;; background color
+(setq frame-background-mode 'dark)
+;; color theme 
+(load-theme 'atom-one-dark t)
+;; font and size
+(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-16"))
+;; auto insert pair
+(electric-pair-mode 1)
+;; highlight pair
+(show-paren-mode 1)
+;; soft word wrap
+(global-visual-line-mode 1)
+;; highlight current line
+(global-hl-line-mode 1)
+;; recent open file
+(recentf-mode 1)
 
-;; evil mode
-;; C-i act as vim
+;; evil-mode
 (setq evil-want-C-i-jump nil)
-;; C-u act as vim
 (setq evil-want-C-u-scroll t)
-(require `evil)
+(require 'evil)
 (evil-mode t)
+
+;; ido-mode
+(require 'ido)
+(ido-mode t)
+
+;; smex-mode
+(global-set-key (kbd "M-x") 'smex)
+
+;; calendar-mode
+(evil-set-initial-state 'calendar-mode 'emacs)
+(setq calendar-week-start-day 1)
+
+;; slime-mode
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
