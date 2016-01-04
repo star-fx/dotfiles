051f6e535bafc2bee847af88f22fb0c4cadb1a4a
star_fx <star.fx27@gmail.com>
2016-01-04 10:49:53 +0800

    a new version

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,56 +1,35 @@
-;; package management
+;; package
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; backup off 
-(setq make-backup-files nil)
-;; autosave off 
-(setq auto-save-default nil)
-;; alarm off
-(setq ring-bell-function 'ignore)
-;; welcome page off
-(setq inhibit-startup-message t)
-;; line number format
-(setq linum-format "%d ")
-;; line number on
-(global-linum-mode t)
-;; toolbar off
-(tool-bar-mode -1)
-;; background color
-(setq frame-background-mode 'dark)
-;; color theme 
-(load-theme 'atom-one-dark t)
-;; font and size
-(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-16"))
-;; auto insert pair
-(electric-pair-mode 1)
-;; highlight pair
-(show-paren-mode 1)
-;; soft word wrap
-(global-visual-line-mode 1)
-;; highlight current line
-(global-hl-line-mode 1)
-;; recent open file
-(recentf-mode 1)
+;; basic
+(setq inhibit-startup-screen t) ;; startup message off
+(setq initial-scratch-message nil) ;; startup scratch empty
 
-;; evil-mode
-(setq evil-want-C-i-jump nil)
-(setq evil-want-C-u-scroll t)
-(require 'evil)
-(evil-mode t)
+(setq make-backup-files nil) ;; backup file off
+(setq auto-save-default nil) ;; auto save off
 
-;; ido-mode
-(require 'ido)
+(global-linum-mode 1) ;; line number on
+(setq linum-format "%d") ;; line number format
+
+(electric-pair-mode t) ;; auto close bracket insertion
+(show-paren-mode t) ;; highlight matching brackets
+
+(global-hl-line-mode t) ;; higlight current line
+
+;; ido
 (ido-mode t)
 
-;; smex-mode
+;; smex
+(smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 
-;; calendar-mode
-(evil-set-initial-state 'calendar-mode 'emacs)
+;; calendar
 (setq calendar-week-start-day 1)
 
-;; slime-mode
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
-(setq slime-contribs '(slime-fancy))
+;; gui
+(menu-bar-mode -1) ;; menu bar off
+(tool-bar-mode -1) ;; tool bar off
+(scroll-bar-mode -1) ;; scroll bar off
